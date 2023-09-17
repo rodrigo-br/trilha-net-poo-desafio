a479c018dcd31763af3e82339f51d5d3e6683583
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-09-16 21:15:50 -0300

    done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("1234", "Modelo 1", "11111aaaa", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Smartphone iphone = new Iphone("4321", "Modelo 2", "bbbb2222", 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
